a6623fb11c5c55989f9c70e9bb961b4a5a0406bd
Lt-Leinad <daniel.feddy0123@gmail.com>
2023-02-26 17:05:30 -0500

    17:05

--- script.js
+++ script.js
@@ -2,16 +2,38 @@ const navbar = document.querySelector(".kfm-logo");
 const section1Text = document.querySelector(".section1-text");
 const slogan = document.querySelector(".slogan");
 const loadingPage = document.querySelector(".loading-page");
+const menu = document.querySelector(".menu");
+const xOut = document.querySelector(".x-out");
+const hamburger = document.querySelector(".hamburger-icon");
+const homeBtn = document.querySelector(".home-btn");
+const content = document.querySelector(".content");
 
 // LOADING SCREEN 
-
-let count = 0;
-function loading() {
-    count++
-    if (count == 5) {
-        document.querySelector(".content").classList.remove("display-none");
-        loadingPage.classList.add("display-none");
+function loadingScreen() {
+    if(!localStorage.getItem("loaded")) {
+        localStorage.setItem("loaded", true);
+        loadingPage.classList.remove("display-none");
+        content.classList.add("display-none");
+        let count = 0;
+    function loading() {
+        count++
+        if (count >= 3) {
+            content.classList.remove("display-none");
+            loadingPage.classList.add("display-none");
+        }
+    };
+    setInterval(loading, 1000);
     }
-};
-setInterval(loading, 1000);
+}
 
+window.addEventListener("load", loadingScreen);
+
+// MENU 
+
+hamburger.addEventListener("click", function() {
+    menu.classList.remove("display-none");
+})
+
+xOut.addEventListener("click", function() {
+    menu.classList.add("display-none");
+})
