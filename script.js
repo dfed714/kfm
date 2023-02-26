833a8021275d57c4f93e34208e2122a56b5d5500
Lt-Leinad <daniel.feddy0123@gmail.com>
2023-02-26 00:17:37 -0500

    00:17

--- script.js
+++ script.js
@@ -3,18 +3,11 @@ const section1Text = document.querySelector(".section1-text");
 const slogan = document.querySelector(".slogan");
 
 function homeTextPadding() {
-    // if (slogan.getBoundingClientRect().y < (navbar.offsetHeight + navbar.getBoundingClientRect().y + 10) && screen.height > 280 && screen.height < screen.width) {
-    //     slogan.style.marginTop = `${(navbar.offsetHeight + navbar.getBoundingClientRect().y + 5)}px`;
-    // } else if (slogan.getBoundingClientRect().y < (navbar.offsetHeight + navbar.getBoundingClientRect().y + 10) && screen.height <= 280 && screen.height < screen.width) {
-    //     slogan.style.marginTop = `${(navbar.offsetHeight + navbar.getBoundingClientRect().y + 75)}px`;
-    // }
-    if (window.innerWidth > window.innerHeight) {
-        slogan.style.color = "red";
+    if (slogan.getBoundingClientRect().y < (navbar.offsetHeight + navbar.getBoundingClientRect().y + 10) && screen.height > 280 && screen.height < screen.width) {
+        slogan.style.marginTop = `${(navbar.offsetHeight + navbar.getBoundingClientRect().y + 5)}px`;
+    } else if (slogan.getBoundingClientRect().y < (navbar.offsetHeight + navbar.getBoundingClientRect().y + 10) && screen.height <= 280 && screen.height < screen.width) {
+        slogan.style.marginTop = `${(navbar.offsetHeight + navbar.getBoundingClientRect().y + 75)}px`;
     }
+    screen.orientation.lock("portrait");
 };
 homeTextPadding();
-
-screen.orientation.addEventListener("change", function() {
-    location.reload();
-    homeTextPadding();
-})
