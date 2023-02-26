14c76e7f6735f5df9e07ff855b84afe84c0da304
Lt-Leinad <daniel.feddy0123@gmail.com>
2023-02-26 00:09:34 -0500

    00:09

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const section1Text = document.querySelector(".section1-text");
 const slogan = document.querySelector(".slogan");
 
 function homeTextPadding() {
-    if (slogan.getBoundingClientRect().y < (navbar.offsetHeight + navbar.getBoundingClientRect().y + 10) && screen.height > 280) {
+    if (slogan.getBoundingClientRect().y < (navbar.offsetHeight + navbar.getBoundingClientRect().y + 10) && screen.height > 280 && screen.height < screen.width) {
         slogan.style.marginTop = `${(navbar.offsetHeight + navbar.getBoundingClientRect().y + 5)}px`;
-    } else if (slogan.getBoundingClientRect().y < (navbar.offsetHeight + navbar.getBoundingClientRect().y + 10) && screen.height <= 280) {
+    } else if (slogan.getBoundingClientRect().y < (navbar.offsetHeight + navbar.getBoundingClientRect().y + 10) && screen.height <= 280 && screen.height < screen.width) {
         slogan.style.marginTop = `${(navbar.offsetHeight + navbar.getBoundingClientRect().y + 75)}px`;
     }
 };
