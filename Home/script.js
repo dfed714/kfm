29d1e1888d692fe3ca8a63c24e7d9b1798ccf07d
Lt-Leinad <daniel.feddy0123@gmail.com>
2023-02-26 17:20:53 -0500

    17:20

--- Home/script.js
+++ Home/script.js
@@ -8,26 +8,6 @@ const hamburger = document.querySelector(".hamburger-icon");
 const homeBtn = document.querySelector(".home-btn");
 const content = document.querySelector(".content");
 
-// LOADING SCREEN 
-function loadingScreen() {
-        localStorage.setItem("loaded", true);
-        loadingPage.classList.remove("display-none");
-        content.classList.add("display-none");
-        let count = 0;
-    function loading() {
-        count++
-        if (count >= 3) {
-            content.classList.remove("display-none");
-            loadingPage.classList.add("display-none");
-        }
-    };
-    setInterval(loading, 1000);
-}
-
-window.addEventListener("load", loadingScreen);
-
-
-
 // MENU 
 
 hamburger.addEventListener("click", function() {
