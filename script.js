5d255e03d05f3df67c828c9aedb0f7ba18f601bb
Lt-Leinad <daniel.feddy0123@gmail.com>
2023-02-25 23:32:16 -0500

    23:32

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ const navbar = document.querySelector(".kfm-logo");
 const section1Text = document.querySelector(".section1-text");
 const slogan = document.querySelector(".slogan");
 
-(function homeTextPadding() {
+function homeTextPadding() {
     console.log(navbar.offsetHeight);
     console.log(navbar.offsetHeight + navbar.getBoundingClientRect().y);
     console.log(slogan.getBoundingClientRect().y);
     if (slogan.getBoundingClientRect().y < (navbar.offsetHeight + navbar.getBoundingClientRect().y + 5)) {
         slogan.style.marginTop = `${(navbar.offsetHeight + navbar.getBoundingClientRect().y + 5)}px`;
     }
-})();
+};
+homeTextPadding();
+
+screen.orientation.addEventListener("change", function(e) {
+    homeTextPadding();
+})
