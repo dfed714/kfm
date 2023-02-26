4a5bc15c96a43c5d6f7ca28bd0fad6e12ce0dd63
Lt-Leinad <daniel.feddy0123@gmail.com>
2023-02-25 23:38:15 -0500

    23:38

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ const section1Text = document.querySelector(".section1-text");
 const slogan = document.querySelector(".slogan");
 
 function homeTextPadding() {
-    console.log(navbar.offsetHeight);
-    console.log(navbar.offsetHeight + navbar.getBoundingClientRect().y);
-    console.log(slogan.getBoundingClientRect().y);
     if (slogan.getBoundingClientRect().y < (navbar.offsetHeight + navbar.getBoundingClientRect().y + 5)) {
         slogan.style.marginTop = `${(navbar.offsetHeight + navbar.getBoundingClientRect().y + 10)}px`;
     }
