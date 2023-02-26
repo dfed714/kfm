aa1bf61a01a80e6b05c564b7ae0d6260c453606f
Lt-Leinad <daniel.feddy0123@gmail.com>
2023-02-26 00:28:46 -0500

    00:28

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const navbar = document.querySelector(".kfm-logo");
-const section1Text = document.querySelector(".section1-text");
-const slogan = document.querySelector(".slogan");
+// const navbar = document.querySelector(".kfm-logo");
+// const section1Text = document.querySelector(".section1-text");
+// const slogan = document.querySelector(".slogan");
 
-function homeTextPadding() {
-    if (slogan.getBoundingClientRect().y < (navbar.offsetHeight + navbar.getBoundingClientRect().y + 10) && screen.height > 280 && screen.height < screen.width) {
-        slogan.style.marginTop = `${(navbar.offsetHeight + navbar.getBoundingClientRect().y + 5)}px`;
-    } else if (slogan.getBoundingClientRect().y < (navbar.offsetHeight + navbar.getBoundingClientRect().y + 10) && screen.height <= 280 && screen.height < screen.width) {
-        slogan.style.marginTop = `${(navbar.offsetHeight + navbar.getBoundingClientRect().y + 75)}px`;
-    }
-    screen.orientation.lock("portrait");
-};
-homeTextPadding();
+// function homeTextPadding() {
+//     if (slogan.getBoundingClientRect().y < (navbar.offsetHeight + navbar.getBoundingClientRect().y + 10) && screen.height > 280) {
+//         slogan.style.marginTop = `${(navbar.offsetHeight + navbar.getBoundingClientRect().y + 5)}px`;
+//     } else if (slogan.getBoundingClientRect().y < (navbar.offsetHeight + navbar.getBoundingClientRect().y + 10) && screen.height <= 280) {
+//         slogan.style.marginTop = `${(navbar.offsetHeight + navbar.getBoundingClientRect().y + 75)}px`;
+//     }
+// };
+// homeTextPadding();
+
+// window.addEventListener("", function() {
+//     location.reload();
+//     homeTextPadding();
+// })
