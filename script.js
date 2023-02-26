099d14299d947b99dcd7bf97cde0231a17e5878d
Lt-Leinad <daniel.feddy0123@gmail.com>
2023-02-26 11:20:00 -0500

    11:19

--- script.js
+++ script.js
@@ -3,24 +3,8 @@ const section1Text = document.querySelector(".section1-text");
 const slogan = document.querySelector(".slogan");
 const loadingPage = document.querySelector(".loading-page");
 
-// GALAXY FOLD RESPONSIVE 
-
-// function homeTextPadding() {
-//     if (slogan.getBoundingClientRect().y < (navbar.offsetHeight + navbar.getBoundingClientRect().y + 10) && screen.height <= 280) {
-//         slogan.style.marginTop = `${(navbar.offsetHeight + navbar.getBoundingClientRect().y + 10)}px`;
-//     } 
-// };
-// homeTextPadding();
-
-// screen.orientation.addEventListener("change", function() {
-//     location.reload();
-//     homeTextPadding();
-// })
-
 // LOADING SCREEN 
 
-
-// setInterval(loading, 500);
 let count = 0;
 function loading() {
     count++
