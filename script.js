34e10ca00711c64ed45613456ac925680c0ec4d9
Lt-Leinad <daniel.feddy0123@gmail.com>
2023-02-27 13:25:47 -0500

    13:25

--- script.js
+++ script.js
@@ -8,22 +8,26 @@ const hamburger = document.querySelector(".hamburger-icon");
 const homeBtn = document.querySelector(".home-btn");
 const content = document.querySelector(".content");
 const footer = document.querySelector(".footer");
+const section2 = document.querySelector(".section2");
+const loadingImg = document.querySelector(".loading-img");
 
 // LOADING SCREEN 
 let loadingCount = 0;
+
+function loading() {
+    loadingCount++
+    if (loadingCount >= 3) {
+        content.classList.remove("display-none");
+        loadingPage.classList.add("display-none");
+        clearInterval(loadingInterval);
+    }
+};
+
+let loadingInterval = setInterval(loading, 1000);
+
 function loadingScreen() {
     if(!sessionStorage.getItem("loaded")) {
         sessionStorage.setItem("loaded", true);
-        function loading() {
-            loadingCount++
-            if (loadingCount >= 3) {
-                content.classList.remove("display-none");
-                loadingPage.classList.add("display-none");
-            }
-        };
-        console.log(loadingCount);
-        setInterval(loading, 1000);
-        clearInterval();
     } else {
         loadingPage.classList.add("display-none");
         content.classList.remove("display-none");
@@ -36,23 +40,22 @@ window.addEventListener("load", loadingScreen);
 
 hamburger.addEventListener("click", hamburgerFunc);
 
-let menuCount = 0;
-
 function hamburgerFunc() {
-    menu.classList.remove("display-none");
-    function menuFunc() {
-        menuCount++;
-        if (menuCount > 0 && !menu.classList.contains("display-none")) {
-            content.classList.add("display-none");
-            footer.classList.add("display-none");
-        } 
-    };
-    console.log(menuCount);
-    setInterval(menuFunc, 100);
+    if (window.innerWidth < 900) {
+        menu.classList.remove("display-none");
+        section2.classList.add("display-none");
+        footer.classList.add("display-none");
+    } else {
+        menu.classList.remove("display-none");
+    }
 }
 
 xOut.addEventListener("click", function() {
-    menu.classList.add("display-none");
-    content.classList.remove("display-none");
-    footer.classList.remove("display-none");
+    if (window.innerWidth < 900) {
+        menu.classList.add("display-none");
+        section2.classList.remove("display-none");
+        footer.classList.remove("display-none");
+    } else {
+        menu.classList.add("display-none");
+    }
 })
