97cdeaaf559edfb866a8b65500a0c82b588e6817
Lt-Leinad <daniel.feddy0123@gmail.com>
2023-02-27 22:54:08 -0500

    22:54

---
+++ Contact Us/script.js
@@ -0,0 +1,31 @@
+const navbar = document.querySelector(".kfm-logo");
+const menu = document.querySelector(".menu");
+const xOut = document.querySelector(".x-out");
+const hamburger = document.querySelector(".hamburger-icon");
+const homeBtn = document.querySelector(".home-btn");
+const content = document.querySelector(".content");
+const footer = document.querySelector(".footer");
+
+
+// MENU 
+
+hamburger.addEventListener("click", hamburgerFunc);
+
+function hamburgerFunc() {
+    if (window.innerWidth < 900) {
+        menu.classList.remove("display-none");
+        footer.classList.add("display-none");
+    } else {
+        menu.classList.remove("display-none");
+    }
+}
+
+xOut.addEventListener("click", function() {
+    if (window.innerWidth < 900) {
+        menu.classList.add("display-none");
+        footer.classList.remove("display-none");
+
+    } else {
+        menu.classList.add("display-none");
+    }
+});
