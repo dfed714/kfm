a4f6c452b6d8922373a72d2e56c14efa0c69549f
Lt.Leinad <daniel.feddy123@gmail.com>
2023-10-22 19:36:18 +0100

    formSubmit fix, animation glitch fix

--- script.js
+++ script.js
@@ -17,75 +17,56 @@ const section1Img = document.querySelector(".section1-image");
 const topList = document.querySelector(".top-list");
 const banner = document.querySelector(".banner");
 
-// DISABLE SCROLLING 
-
-function disableScrolling() {
-    document.documentElement.style.overflowY = "clip";
-}
-
-// ENABLE SCROLLING 
-
-function enableScrolling() {
-    document.documentElement.style.overflowY = "auto";
-}
-
-// LOADING SCREEN 
+// LOADING SCREEN
 
 let loadingCount = 0;
 let loadingInterval = setInterval(loading, 1000);
 
 function loading() {
-    loadingCount++
-        if (loadingCount >= 3) {
-            // enableScrolling();
-            loadingPage.classList.add("display-none");
-            clearInterval(loadingInterval);
-        } 
-};
-
+  loadingCount++;
+  if (loadingCount >= 3) {
+    loadingPage.classList.add("display-none");
+    clearInterval(loadingInterval);
+  }
+}
 
 function loadingScreen() {
-    if(!sessionStorage.getItem("loaded")) {
-        section1Text.style.animationDelay = "2000ms";
-        section1Img.style.animationDelay = "2000ms";
-        topList.style.animationDelay = "2000ms";
-        sessionStorage.setItem("loaded", true);
-        // disableScrolling();
-    } else {
-        loadingPage.classList.add("display-none");
-        // enableScrolling();
-        section1Text.style.animationDelay = "0";
-        section1Img.style.animationDelay = "0";
-        topList.style.animationDelay = "0"
-    }
+  if (!sessionStorage.getItem("loaded")) {
+    sessionStorage.setItem("loaded", true);
+  } else {
+    loadingPage.classList.add("display-none");
+  }
 }
 
 window.addEventListener("load", loadingScreen);
 
-// MENU 
+// MENU
 
 hamburger.addEventListener("click", hamburgerFunc);
 
 function hamburgerFunc() {
-    if (window.innerWidth < 900) {
-        menu.classList.remove("display-none");
-        disableScrolling();
-    } else {
-        menu.classList.remove("display-none");
-    }
+  if (window.innerWidth < 900) {
+    menu.classList.remove("display-none");
+    disableScrolling();
+  } else {
+    menu.classList.remove("display-none");
+  }
 }
 
-xOut.addEventListener("click", function() {
-    if (window.innerWidth < 900) {
-        menu.classList.add("display-none");
-        enableScrolling();
-    } else {
-        menu.classList.add("display-none");
-    }
+xOut.addEventListener("click", function () {
+  if (window.innerWidth < 900) {
+    menu.classList.add("display-none");
+    enableScrolling();
+  } else {
+    menu.classList.add("display-none");
+  }
 });
 
-// GET IN TOUCH MODAL 
+// GET IN TOUCH MODAL
 
-getInTouch.addEventListener("click", function() {
-    window.scrollTo(0, banner.getBoundingClientRect().top + ((window.innerHeight/100) * 6.25));
-})
+getInTouch.addEventListener("click", function () {
+  window.scrollTo(
+    0,
+    banner.getBoundingClientRect().top + (window.innerHeight / 100) * 6.25
+  );
+});
